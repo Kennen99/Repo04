111462103af2428d0287d8c55da8ec6d6c0341ac
Kennen99 <kenanmusanovic99@gmail.com>
2021-09-16 23:50:42 +0200

    Added a js file in ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    // only change code below this line
+    var myFirstName = "Kenan";
+    var myLastName = "Mušanović";
+    var myHomeTown = "Sarajevo";
+    // only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
