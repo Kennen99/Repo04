6b13fe51ec03b1d2595e66926c232c5cfbda5b1d
Kennen99 <kenanmusanovic99@gmail.com>
2021-09-16 23:16:30 +0200

    Added a js file in ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,9 @@
+function main(numOne, numTwo){
+    // only change code below this line
+    var remainder = numOne % numTwo;
+    // only change code above this line
+    return remainder;
+}
+
+console.log(main(111, 24));
+module.exports = main;
